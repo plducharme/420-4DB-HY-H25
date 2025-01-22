91e2d745ed205a4c8e23743a9f702a7c91a19626
Pier Luc Ducharme <dutch80@gmail.com>
2025-01-22 18:01:06 -0500

    ajout exemple EF_Projet1

---
+++ Semaine 1/EF_Projet1/EF_Projet1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Projet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Création du context");
+            Entities context = new Entities();
+
+            //Contact contact = context.Contacts.FirstOrDefault(c => c.ContactID == 367);
+            List<Contact> contacts = context.Contacts.ToList();
+            foreach(Contact c in contacts)
+            { 
+                Console.WriteLine(c.ContactID + " - " + c.FirstName + c.LastName);
+                foreach (Projet p in c.Projets)
+                    Console.WriteLine("Description Projet : " + p.Description);
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
