b3c647145aa02eaeb030f51dc6e692e41e8ce196
Pier Luc Ducharme <dutch80@gmail.com>
2025-02-26 21:59:02 -0500

    Ajout réponses LINQ semaine 3

---
+++ Semaine 3/Exercices/JeuxOlympiques2025/JeuxOlympiques2025/Commanditaire.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JeuxOlympiques2025;
+
+public partial class Commanditaire
+{
+    public int Id { get; set; }
+
+    public string Nom { get; set; } = null!;
+
+    public decimal CommanditeParCoureur { get; set; }
+
+    public virtual ICollection<Coureur> IdCoureurs { get; set; } = new List<Coureur>();
+}
