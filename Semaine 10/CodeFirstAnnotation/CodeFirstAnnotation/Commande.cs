48381d9971ee57277bc486d97b26ff1e242aa997
Pier Luc Ducharme <dutch80@gmail.com>
2025-04-17 14:20:44 -0400

    ajout exemple CodeFirstAnnotation

---
+++ Semaine 10/CodeFirstAnnotation/CodeFirstAnnotation/Commande.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstAnnotation
+{
+    public class Commande
+    {
+
+
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [ForeignKey("CleAvecNomBizarre")]
+        public Client Client { get; set; }
+        public List<Produit> Produits { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCommande { get; set; }
+        public Statut Statut { get; set; }
+
+    }
+}
---
+++ Semaine 10/CodeFirstAnnotation/CodeFirstAnnotation/Statut.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstAnnotation
+{
+    
+    public enum Statut
+    {
+        [Description("Commande soumis")]
+        SOUMIS,
+        [Description("Commande en traitement")]
+        EN_TRAITEMENT,
+        [Description("Terminée")]
+        TERMINE,
+        [Description("Annulée")]
+        ANNULEE
+    }
+}
